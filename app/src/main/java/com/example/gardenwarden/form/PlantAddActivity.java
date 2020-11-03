4d904212f630845c8503f795c9c93c635dd82133
SprytnyModrzew <kacwaj@gmail.com>
2020-11-03 13:08:33 +0100

    animations added

--- app/src/main/java/com/example/gardenwarden/form/PlantAddActivity.java
+++ app/src/main/java/com/example/gardenwarden/form/PlantAddActivity.java
@@ -3,8 +3,10 @@ package com.example.gardenwarden.form;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -18,12 +20,28 @@ import com.example.gardenwarden.DefaultPlantFragment;
 import com.example.gardenwarden.db.plantdefault.PlantDefaultCategory;
 import com.example.gardenwarden.device.DeviceFormFragment;
 import com.example.gardenwarden.device.DeviceFragment;
+import com.example.gardenwarden.ui.main.FormFragment;
 
-public class PlantAddActivity extends AppCompatActivity implements DeviceFormFragment.OnListFragmentInteractionListener,DefaultPlantFragment.OnListFragmentInteractionListener, PlantSubDefaultFragment.OnListFragmentInteractionListener {
+public class PlantAddActivity extends AppCompatActivity implements
+        DeviceFormFragment.OnListFragmentInteractionListener,
+        DefaultPlantFragment.OnListFragmentInteractionListener,
+        PlantSubDefaultFragment.OnListFragmentInteractionListener,
+        FormFragment.OnButtonClicked {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plant);
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull Fragment fragment) {
+
+        if (fragment instanceof FormFragment) {
+            FormFragment headlinesFragment = (FormFragment) fragment;
+            headlinesFragment.setClickedListener(this);
+            return;
+        }
+        super.onAttachFragment(fragment);
     }
 
     @Override
@@ -32,6 +50,12 @@ public class PlantAddActivity extends AppCompatActivity implements DeviceFormFra
         //todo zamiast device'ów, jakieś nowe deviceAvailable zrobić
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(
+                R.anim.slide_in,  // enter
+                R.anim.fade_out,  // exit
+                R.anim.fade_in,   // popEnter
+                R.anim.slide_out  // popExit
+        );
 
         PlantSubDefaultFragment blankFragment = new PlantSubDefaultFragment(item.getId());
 
@@ -53,7 +77,23 @@ public class PlantAddActivity extends AppCompatActivity implements DeviceFormFra
 
     @Override
     public void onListFragmentInteraction(PlantDefaultCategory item) {
-        Log.e("click","click");
+        Log.e("clicked","eo");
+        //todo zamiast device'ów, jakieś nowe deviceAvailable zrobić
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FormFragment blankFragment = new FormFragment();
+        fragmentTransaction.setCustomAnimations(
+                R.anim.slide_in,  // enter
+                R.anim.fade_out,  // exit
+                R.anim.fade_in,   // popEnter
+                R.anim.slide_out  // popExit
+        );
+
+        fragmentTransaction.replace(R.id.plant_add_fragment,blankFragment);
+        fragmentTransaction.addToBackStack(null);
+
+        fragmentTransaction.commit();
     }
 
     @Override
@@ -71,6 +111,12 @@ public class PlantAddActivity extends AppCompatActivity implements DeviceFormFra
         Log.e("clicked","eo");
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(
+                R.anim.slide_in,  // enter
+                R.anim.fade_out,  // exit
+                R.anim.fade_in,   // popEnter
+                R.anim.slide_out  // popExit
+        );
 
         DefaultPlantFragment blankFragment = new DefaultPlantFragment();
 
@@ -88,5 +134,10 @@ public class PlantAddActivity extends AppCompatActivity implements DeviceFormFra
     @Override
     public void onButtonClick(Device mItem) {
 
+    }
+
+    @Override
+    public void onAddButtonClick() {
+        Log.e("działa","auu");
     }
 }
